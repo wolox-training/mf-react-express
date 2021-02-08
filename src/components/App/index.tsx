e63b0d69b7d1b7a1f9f74dbc9c1a46a16f2ce109
Manuela <manuela.fernandez@wolox.com.ar>
2021-02-08 17:58:01 -0300

    Add loader - Suspense

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 import 'scss/application.scss';
 import SignUp from 'screens/SignUp';
+import CustomSuspense from 'components/Suspense';
 
 function App() {
-  return <SignUp />;
+  return (
+    <CustomSuspense>
+      <SignUp />
+    </CustomSuspense>
+  );
 }
 
 export default App;
