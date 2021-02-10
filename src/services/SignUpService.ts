fcc32462a03840fb5ef07d2d05940afd1e69f8b2
Manuela <manuela.fernandez@wolox.com.ar>
2021-02-10 11:55:25 -0300

    Change api call

--- src/services/SignUpService.ts
+++ src/services/SignUpService.ts
@@ -1,13 +1,13 @@
 import { create } from 'apisauce';
 
-const api = create({ baseURL: 'https://books-training-rails.herokuapp.com/' });
+const api = create({ baseURL: 'https://books-training-rails.herokuapp.com' });
 
 export interface IFormInput {
   email: string;
   firstName: string;
   lastName: string;
   password: string;
-  passwordConfirmation: string;
+  passwordConfirmation?: string;
 }
 
-export const signUpService = (value: IFormInput) => api.post('/', value);
+export const signUpService = (value: IFormInput) => api.post('/api/v1/users', value);
