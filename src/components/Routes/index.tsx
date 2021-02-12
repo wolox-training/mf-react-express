de2511fe56ef5a53007d955cb37c141bdfaa600b
Manuela <manuela.fernandez@wolox.com.ar>
2021-02-12 12:28:18 -0300

    Delete CustomSuspense

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import CustomSuspense from 'components/Suspense';
 import SignUp from 'screens/SignUp';
 import Login from 'screens/Login';
 
 function Routes() {
   return (
     <BrowserRouter>
-      <CustomSuspense>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/sign_up" component={SignUp} />
-        </Switch>
-      </CustomSuspense>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/sign_up" component={SignUp} />
+      </Switch>
     </BrowserRouter>
   );
 }
