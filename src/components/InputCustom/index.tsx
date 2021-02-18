c0ab85840740ea90555c8a343a9a3d9bba48136a
Manuela <manuela.fernandez@wolox.com.ar>
2021-02-18 12:32:56 -0300

    Change div to Fragment

--- src/components/InputCustom/index.tsx
+++ src/components/InputCustom/index.tsx
@@ -13,11 +13,11 @@ interface Props {
 
 function InputCustom({ error, label, inputClassName, inputRef, inputType, name }: Props) {
   return (
-    <div>
+    <>
       <label className={styles.appLabel}>{label} </label>
       <input type={inputType} className={`${styles.appInput} ${inputClassName}`} name={name} ref={inputRef} />
       {error && <span className={styles.appLabelError}>{error}</span>}
-    </div>
+    </>
   );
 }
 
