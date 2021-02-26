593dad2bf354639d08e8ca4d523491a4fa011dd6
Manuela <manuela.fernandez@wolox.com.ar>
2021-02-26 12:06:15 -0300

    Fix InputCustom component

--- src/components/InputCustom/index.tsx
+++ src/components/InputCustom/index.tsx
@@ -5,18 +5,19 @@ import styles from './styles.module.scss';
 interface Props {
   error?: string;
   label: React.ReactNode;
+  inputClassName?: string;
   inputRef: any;
   inputType: string;
   name: string;
 }
 
-function InputCustom({ error, label, inputRef, inputType, name }: Props) {
+function InputCustom({ error, label, inputClassName, inputRef, inputType, name }: Props) {
   return (
     <>
       <label className={styles.appLabel}>{label}</label>
       <input
         type={inputType}
-        className={`${styles.appInput} ${error ? styles.error : ''}`}
+        className={`${styles.appInput} ${error ? styles.error : ''} ${inputClassName}`}
         name={name}
         aria-label={name}
         ref={inputRef}
